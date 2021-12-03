676f22bbb97acdb426055ec474f1fa8dbb8f8245
Daniel Tsivkovski <dtsivkovs@gmail.com>
2021-12-03 13:16:30 -0800

    added routing testing

--- routes/mainframe.py
+++ routes/mainframe.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request
+# flask imports
+from flask import Blueprint, render_template, request, url_for, redirect, jsonify, make_response
+# model imports
+#from .model import model_create, model_read, model_read_all, model_read_emails, \
+#    model_read_phones, model_update_name, model_delete, model_read_by_filter
 
 app_mainframe = Blueprint('mainframe', __name__,
                      url_prefix='/mainframe',
@@ -10,9 +14,15 @@ app_mainframe = Blueprint('mainframe', __name__,
 def mainsearch():
     return render_template("mainframe/full_search.html")
 
-@app_mainframe.route('/search/term')
-def termsearch():
-    
-    return render_template("mainframe/full_search.html")
+@app_mainframe.route('/search/term/', methods=["POST"])
+def search_term():
+    req = request.get_json()
+    term = req['term']
+    #query = model_read_by_filter(term)
+    #response = make_response(jsonify(query), 200)
+    response = term + " - The route is working" 
+    print(response)
+    # test route connection by seeing console output
+    return response
 
 
