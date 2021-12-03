106d3349bc8de2e017dc61cfdecec0ec5c4dea0e
Daniel Tsivkovski <dtsivkovs@gmail.com>
2021-12-02 20:05:19 -0800

    main.py routing fix

--- main.py
+++ main.py
@@ -1,48 +1,31 @@
-# import "packages" from flask
 from flask import Flask, render_template
 
 # create a Flask instance
 app = Flask(__name__)
 
+from routes.apis import app_apis
+from routes.danroutes import app_danroutes
+from routes.lucroutes import app_lucroutes
+from routes.ritroutes import app_ritroutes
+from routes.junroutes import app_junroutes
+from routes.evroutes import app_evroutes
+
+app.register_blueprint(app_apis)
+app.register_blueprint(app_danroutes)
+app.register_blueprint(app_lucroutes)
+app.register_blueprint(app_ritroutes)
+app.register_blueprint(app_junroutes)
+app.register_blueprint(app_evroutes)
 
 # connects default URL to render index.html
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/everitt/about')
-def everitt():
-    return render_template("everitt/e_aboutme.html")
-
-@app.route('/daniel/home')
-def daniel():
-    return render_template("daniel/d_homepage.html")
-
-@app.route('/daniel/testing1')
-def danieltest():
-    return render_template("daniel/csstesting.html")
-
-@app.route('/rithwikh/home')
-def rithwikh():
-    return render_template("rithwikh/r_homepage.html")
-
-@app.route('/jun')
-def jun():
-    return render_template("jun/j_aboutme.html")
-
-@app.route('/lucas/home')
-def lucas():
-    return render_template("lucas/l_homepage.html")
-
-# connects /kangaroos path to render kangaroos.html
-
-
-
 @app.route('/stub/')
 def stub():
     return render_template("stub.html")
-
-
+    
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(debug=True)
