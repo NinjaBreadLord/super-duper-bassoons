1cfe35e2a6a458e75c4f6af074a394c38790d593
Daniel Tsivkovski <dtsivkovs@gmail.com>
2021-12-03 13:58:49 -0800

    added model.py framework for future testing

--- routes/mainframe.py
+++ routes/mainframe.py
@@ -1,8 +1,8 @@
 # flask imports
 from flask import Blueprint, render_template, request, url_for, redirect, jsonify, make_response
 # model imports
-#from .model import model_create, model_read, model_read_all, model_read_emails, \
-#    model_read_phones, model_update_name, model_delete, model_read_by_filter
+from .model import model_create, model_read, model_read_all, model_read_emails, \
+    model_read_phones, model_update_name, model_delete, model_read_by_filter
 
 app_mainframe = Blueprint('mainframe', __name__,
                      url_prefix='/mainframe',
@@ -18,11 +18,9 @@ def mainsearch():
 def search_term():
     req = request.get_json()
     term = req['term']
-    #query = model_read_by_filter(term)
-    #response = make_response(jsonify(query), 200)
-    response = term + " - The route is working" 
-    print(response)
-    # test route connection by seeing console output
+    query = model_read_by_filter(term) 
+    response = make_response(jsonify(query), 200) 
+    print(response) 
     return response
 
 
