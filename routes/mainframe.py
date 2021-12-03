5595536d65762822bdeee84a6364d7a9efa50929
Daniel Tsivkovski <dtsivkovs@gmail.com>
2021-12-02 19:46:33 -0800

    temporary searching framework created

---
+++ routes/mainframe.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, request
+
+app_mainframe = Blueprint('mainframe', __name__,
+                     url_prefix='/mainframe',
+                     template_folder='templates/mainframe/',
+                     static_folder='static',
+                     static_url_path='assets')
+
+@app_mainframe.route('/search')
+def mainsearch():
+    return render_template("mainframe/full_search.html")
+
