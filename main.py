2b7d75e92a9a81d300e1158f82b367148b00cf53
rithwikhvarma <rithwikh.v@gmail.com>
2022-01-03 13:06:54 -0800

    ∆˜ˆˆç∆ˆˆª£º•¶£¢•ªç∆ˆˆ∆˜˜ßåç∫©¥ˆ˜˜å≈¬≤çø

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from routes.lucroutes import app_lucroutes
 from routes.ritroutes import app_ritroutes
 from routes.junroutes import app_junroutes
 from routes.evroutes import app_evroutes
-from mainframe.mainframe import app_mainframe
+
 from routes.mainsite import app_mainsite
 
 app.register_blueprint(app_mainsite)
@@ -22,7 +22,7 @@ app.register_blueprint(app_lucroutes)
 app.register_blueprint(app_ritroutes)
 app.register_blueprint(app_junroutes)
 app.register_blueprint(app_evroutes)
-app.register_blueprint(app_mainframe)
+
 
 # connects default URL to render index.html
 @app.route('/')
