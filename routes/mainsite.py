b4734501484a34a68a57cac094ef4fc0eaf682a9
rithwikhvarma <rithwikh.v@gmail.com>
2022-01-06 12:50:51 -0800

    ∆˜ˆˆç∆ˆˆª£º•¶£¢•ªç∆ˆˆ∆˜˜ßåç∫©¥ˆ˜˜å≈¬≤çø

--- routes/mainsite.py
+++ routes/mainsite.py
@@ -1,19 +1,19 @@
 from flask import Blueprint, render_template, request
 
 app_mainsite = Blueprint('mainsite', __name__,
-                     url_prefix='/shop/',
+                     url_prefix='/store/',
                      template_folder='templates/',
                      static_folder='static',
                      static_url_path='assets')
 
 @app_mainsite.route('/teaShop/')
 def teaShop():
-    return render_template("shop 1.0 smth idk/teaShop.html")
+    return render_template("store/teaShop.html")
 
 @app_mainsite.route('/teeShop/')
 def teeShop():
-    return render_template("shop 1.0 smth idk/teeShop.html")
+    return render_template("store/teeShop.html")
 
 @app_mainsite.route('/nike/')
 def nike():
-    return render_template("teeShop.html")
+    return render_template("NikeShirt.html")
