71a4e1e27bba139ddeb49fc38d7d3c832486cbbe
lucasho22 <77864093+lucasho22@users.noreply.github.com>
2022-02-26 22:07:57 -0800

    shirt search for create task and transparent long sleeve

--- techtalks/TT5/baef afkfek sumogan.py
+++ techtalks/TT5/baef afkfek sumogan.py
@@ -1,2 +1,24 @@
+def swap():
+    age1 = 21
+    age2 = 16
 
+    print(age1,age2)
+    temp = age1
+    age1 = age2
+    age2 = temp
+    print(age1, age2)
+
+swap()
+
+def swap2():
+    food = "burber"
+    foodd = "pizza"
+
+    print(food,foodd)
+    temp = food
+    food = foodd
+    foodd = temp
+    print(food,foodd)
+
+swap2()
 
